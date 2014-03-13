c3df74d287c0f1506f329612ac7e8e7f88a9d27b
Travis Choma <travischoma@gmail.com>
2014-03-12 17:07:54 -0700

    revert the circular carousel code, not necessary to build this right now

--- dist/angular-carousel.js
+++ dist/angular-carousel.js
@@ -29,23 +29,15 @@ angular.module('angular-carousel')
     },
     link: function(scope, element, attrs) {
       scope.prev = function() {
-        if(sope.index === 0) {
-          scope.index = scope.items.length-1;
-        } else {  
-          scope.index--;
-        }
+        scope.index--;
       };
       scope.next = function() {
-        if(scope.index === scope.items.length-1) {
-          scope.index=0;
-        } else {   
-          scope.index++;
-        }   
+        scope.index++;
       };
     },
     template: '<div class="rn-carousel-controls">' +
-                '<span class="rn-carousel-control rn-carousel-control-prev" ng-click="prev()"></span>' +
-                '<span class="rn-carousel-control rn-carousel-control-next" ng-click="next()"></span>' +
+                '<span class="rn-carousel-control rn-carousel-control-prev" ng-click="prev()" ng-if="index > 0"></span>' +
+                '<span class="rn-carousel-control rn-carousel-control-next" ng-click="next()" ng-if="index < items.length - 1"></span>' +
               '</div>'
   };
 }]);
